d40f177896f38798a61ed4822427b471850c39d6
Daria <dasha.fbi@gmail.com>
2022-03-16 21:50:21 +0100

    footer: create layout

--- layout/footer/index.tsx
+++ layout/footer/index.tsx
@@ -1,8 +1,16 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import cn from 'classnames';
+
 import styles from './Footer.module.css';
 
 interface IFooterProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-export const Footer = (props: IFooterProps) => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props}: IFooterProps) => {
+  return (
+    <footer {...props} className={cn(className, styles.wrapper)}>
+      <p className={styles.year}>OwlTop © 2020 - {new Date().getFullYear()} All rights reserved</p>
+      <p className={styles.terms}>Customer terms</p>
+      <p className={styles.privacy}>Privacy policy</p>
+    </footer>
+  );
 };
