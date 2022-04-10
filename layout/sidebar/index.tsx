57f56b0ab695b5c7e37c3f9ae7f7cda633288a44
Daria <dasha.fbi@gmail.com>
2022-04-10 22:53:14 +0200

    style sidebar

--- layout/sidebar/index.tsx
+++ layout/sidebar/index.tsx
@@ -1,10 +1,18 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
-// import styles from './Sidebar.module.css';
+import styles from './Sidebar.module.css';
 import Menu from '../menu';
+import Logo from '../logo.svg';
+import cn from 'classnames';
 
 interface ISidebarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-export const Sidebar = (props: ISidebarProps) => {
-  return <div {...props}><Menu /></div>;
+export const Sidebar = ({ className, ...props }: ISidebarProps) => {
+  return (
+    <div {...props} className={cn(className, styles.sidebar)}>
+      <Logo className={styles.logo} />
+      <div>Search</div>
+      <Menu />
+    </div>
+  );
 };
