8d5eec6cf42304c2a258030db673d2ec3a30214a
Daria <dasha.fbi@gmail.com>
2022-09-10 20:33:29 +0300

    add next Image component

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -1,4 +1,5 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import Image from 'next/image';
 import cn from 'classnames';
 
 import styles from './Product.module.css';
@@ -14,7 +15,12 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
   return (
     <Card className={cn(className, styles.product)} {...props}>
       <div className={styles.logo}>
-        <img src={`${process.env.NEXT_PUBLIC_DOMAIN}${product.image}`} alt={product.title} />
+        <Image
+          src={`${process.env.NEXT_PUBLIC_DOMAIN}${product.image}`}
+          alt={product.title}
+          width={70}
+          height={70}
+        />
       </div>
       <div className={styles.title}>{product.title}</div>
       <div className={styles.price}>
