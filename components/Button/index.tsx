681f482ccd24f2c0b0873b6e069cfabd8a0d3cf5
Daria <dasha.fbi@gmail.com>
2023-04-02 01:12:40 +0300

    fix colors

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,25 +1,20 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
 import cn from 'classnames';
+import { motion } from 'framer-motion';
 
 import styles from './Button.module.css';
 import ArrowIcon from './ArrowIcon.svg';
 
-interface IButtonProps
-  extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+interface IButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   children: ReactNode;
   appearance: 'primary' | 'ghost';
   arrow?: 'right' | 'down' | 'none';
 }
 
-export const Button = ({
-  appearance,
-  arrow = 'none',
-  children,
-  className,
-  ...props
-}: IButtonProps) => {
+export const Button = ({ appearance, arrow = 'none', children, className, ...props }: IButtonProps) => {
   return (
-    <button
+    <motion.button
+      whileHover={{ scale: 1.05 }}
       className={cn(styles.button, className, {
         [styles.primary]: appearance === 'primary',
         [styles.ghost]: appearance === 'ghost',
@@ -32,6 +27,6 @@ export const Button = ({
           <ArrowIcon />
         </span>
       )}
-    </button>
+    </motion.button>
   );
 };
