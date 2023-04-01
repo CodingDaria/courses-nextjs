d2a5cf53b6cc1ce83b83b337d52376310d90bd51
Daria <dasha.fbi@gmail.com>
2023-04-01 16:30:52 +0300

    feat: add mobile menu

--- layout/header/index.tsx
+++ layout/header/index.tsx
@@ -1,8 +1,50 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import { DetailedHTMLProps, HTMLAttributes, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import cn from 'classnames';
+import { motion } from 'framer-motion';
+
 import styles from './Header.module.css';
+import Logo from '../logo.svg';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { Sidebar } from '../sidebar';
 
 interface IHeaderProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-export const Header = (props: IHeaderProps) => {
-  return <div {...props}>Header</div>;
+export const Header = ({ className, ...props }: IHeaderProps) => {
+  const [isOpened, setOpened] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setOpened(false);
+  }, [router]);
+
+  const variants = {
+    opened: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        stiffness: 20,
+      },
+    },
+    closed: {
+      opacity: 0,
+      x: '100%',
+    },
+  };
+
+  return (
+    <header className={cn(className, styles.header)} {...props}>
+      <Logo />
+      <ButtonIcon appearance="white" icon="menu" onClick={() => setOpened(true)} />
+      <motion.div
+        className={styles.mobileMenu}
+        variants={variants}
+        initial={isOpened ? 'opened' : 'closed'}
+        animate={isOpened ? 'opened' : 'closed'}
+      >
+        <Sidebar />
+        <ButtonIcon className={styles.menuClose} appearance="white" icon="close" onClick={() => setOpened(false)} />
+      </motion.div>
+    </header>
+  );
 };
