848242810b3f7353f7727536fdb59bc55a17bdb2
Daria <dasha.fbi@gmail.com>
2023-04-08 21:39:12 +0300

    fix sort buttons

--- components/Sort/index.tsx
+++ components/Sort/index.tsx
@@ -12,22 +12,22 @@ interface SortProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HT
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
   return (
     <div className={cn(className, styles.sort)} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({
           [styles.active]: sort === SortEnum.Rating,
         })}
       >
         <SortIcon className={styles.sortIcon} /> By rating
-      </span>
-      <span
+      </button>
+      <button
         onClick={() => setSort(SortEnum.Price)}
         className={cn({
           [styles.active]: sort === SortEnum.Price,
         })}
       >
         <SortIcon className={styles.sortIcon} /> By price
-      </span>
+      </button>
     </div>
   );
 };
